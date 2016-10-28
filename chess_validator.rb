4e0e6aeb0c6bad77082c15ecf44bb4770cb6b11f
Jimeno0 <carlospj@icloud.com>
2016-10-28 15:55:27 +0200

    all moves dones and shared moves refactorished

--- chess_validator.rb
+++ chess_validator.rb
@@ -1,66 +1,23 @@
 require "pry"
 
-class Board
-  def initialize(arr_of_arr)
-    @board = arr_of_arr
-  end
-end
-
-class Position_translator
-  def self.translate(string)
-    string_arr = string.chars
-    xpos = string_arr[0].ord - 97
-    ypos =  8 - string_arr[1].to_i
-    [xpos,ypos]
-  end
-end
-
-
-class ChessPiece
-  def initialize
-    @available_moves = []
-  end
-end
-
-class Pawn < ChessPiece
-  def can_move_to(from)
-    @available_moves.push(from[0],from[1]-1)
-    @available_moves
-  end
-end
-
-class King < ChessPiece
-  def can_move_to(from)
-
-    for x in (from[0]-1)..(from[0]+1)
-      for y in (from[1]-1)..(from[1]+1)  
-        @available_moves.push([x,y])        
-      end
-    end
-    @available_moves.delete(from)
-    @available_moves
-  end
-end
-
-class Rook < ChessPiece
-  def can_move_to(from)
+#SHARED MOVEMENT MODULES
+module Rook_move
+  def rook_moves(from)
     for y in 0..7
       @available_moves.push([from[0],y])
       @available_moves.delete(from)
     end
 
     for x in 0..7
-      @available_moves.push([x,from[0]])
+      @available_moves.push([x,from[1]])
       @available_moves.delete(from)
     end
     @available_moves
   end
 end
 
-
-class Bishop < ChessPiece
-
-  def can_move_to(from)
+module Bishop_move
+  def bishop_moves(from)
     dif = from[0] - from[1]
     sum = from[0] + from[1]
 
@@ -87,18 +44,63 @@ class Bishop < ChessPiece
   end
 end
 
+
+class Board
+  def initialize(arr_of_arr)
+    @board = arr_of_arr
+  end
+end
+
+class Position_translator
+  def self.translate(string)
+    string_arr = string.chars
+    xpos = string_arr[0].ord - 97
+    ypos =  8 - string_arr[1].to_i
+    [xpos,ypos]
+  end
+end
+
+
+class ChessPiece
+  def initialize
+    @available_moves = []
+  end
+end
+
+class Pawn < ChessPiece
+  def get_moves(from)
+    @available_moves.push(from[0],from[1]-1)
+    @available_moves
+  end
+end
+
+class King < ChessPiece
+  def get_moves(from)
+
+    for x in (from[0]-1)..(from[0]+1)
+      for y in (from[1]-1)..(from[1]+1)  
+        @available_moves.push([x,y])        
+      end
+    end
+    @available_moves.delete(from)
+    @available_moves
+  end
+end
+
 class Knight < ChessPiece
 
-  def can_move_to(from)
-    pos1 = [from[0]-2,from[0]-1]
-    pos2 = [from[0]-2,from[0]+1]
-    pos3 = [from[0]-1,from[0]-2]
-    pos4 = [from[0]-1,from[0]+2]
+  def get_moves(from)
+    
 
-    pos5 = [from[0]+1,from[0]-2]
-    pos6 = [from[0]+1,from[0]+2]
-    pos7 = [from[0]+2,from[0]-1]
-    pos8 = [from[0]+2,from[0]+1]
+    pos1 = [from[0]-2,from[1]-1]
+    pos2 = [from[0]-2,from[1]+1]
+    pos3 = [from[0]-1,from[1]-2]
+    pos4 = [from[0]-1,from[1]+2]
+
+    pos5 = [from[0]+1,from[1]-2]
+    pos6 = [from[0]+1,from[1]+2]
+    pos7 = [from[0]+2,from[1]-1]
+    pos8 = [from[0]+2,from[1]+1]
 
     positions = [pos1,pos2,pos3,pos4,pos5,pos6,pos7,pos8]
     positions.each do |pos|
@@ -109,19 +111,44 @@ class Knight < ChessPiece
   end
 end
 
+class Rook < ChessPiece
+  include Rook_move
 
+  def get_moves(from)
+    rook_moves(from)
+  end
+end
+
+
+class Bishop < ChessPiece
+  include Bishop_move
+
+  def get_moves(from)
+    bishop_moves(from)
+  end
+end
+
+class Queen <ChessPiece
+  include Rook_move
+  include Bishop_move
+
+  def get_moves(from)
+    rook_moves(from)
+    bishop_moves(from)
+  end
+end
 
 # Comprobar cuando comen, el peon, si es blanca o negra
 #si se sale del tablero el moviemiento, podria ser que vaya a a entrar 
 #por el otro extremo del tablero en el peon y rey y eso hay que arreglarlo tambien (dejar para el final la comparacion con si hay otra ficha, así podemos hacer que no pete???)
 
 
-# my_pawn = Pawn.new
-# my_king = King.new
-# my_rook = Rook.new
-# my_bishop = Bishop.new
+my_pawn = Pawn.new
+my_king = King.new
+my_rook = Rook.new
+my_bishop = Bishop.new
 my_knight = Knight.new
-
+my_queen = Queen.new
 
 # g3 ---> 6,3
 # c7 ---> 2,1
@@ -137,11 +164,12 @@ my_knight = Knight.new
 puts Position_translator.translate("c7")
 
 puts  "-------"
-# puts my_pawn.can_move_to(Position_translator.translate("g3"))
-# puts my_king.can_move_to(Position_translator.translate("g3"))
-# puts my_rook.can_move_to(Position_translator.translate("g3"))
-# puts my_bishop.can_move_to(Position_translator.translate("c7"))
-puts my_knight.can_move_to(Position_translator.translate("h8"))
+my_pawn.get_moves(Position_translator.translate("c7"))
+my_king.get_moves(Position_translator.translate("c7"))
+my_rook.get_moves(Position_translator.translate("c7"))
+my_bishop.get_moves(Position_translator.translate("c7"))
+my_knight.get_moves(Position_translator.translate("c7"))
+my_queen.get_moves(Position_translator.translate("c7"))
 #Sample
 board = [
           ["a8","b8","c8","d8","e8","f8","g8","h8"],
